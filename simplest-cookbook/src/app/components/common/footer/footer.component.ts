e9fcc6840e4b90bea30a83de962cfd3b0a53904d
John Montanye <gatewayitconsulting@outlook.com>
2019-08-18 00:30:42 -0500

    Fixed Navigation and Changed Color.

--- simplest-cookbook/src/app/components/common/footer/footer.component.ts
+++ simplest-cookbook/src/app/components/common/footer/footer.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  year: number = Date.now();
+  
   constructor() { }
 
   ngOnInit() {
   }
-
-  year: number = Date.now();
 
 }
