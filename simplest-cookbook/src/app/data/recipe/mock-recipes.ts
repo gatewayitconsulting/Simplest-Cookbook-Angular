46d858c9132274a5ac794ababc9e4ae91475b5f0
gatewayitconsultingDesktop <43389272+gatewayitconsulting@users.noreply.github.com>
2019-11-12 19:54:30 -0600

    Increased responsiveness of recipe card component

--- simplest-cookbook/src/app/data/recipe/mock-recipes.ts
+++ simplest-cookbook/src/app/data/recipe/mock-recipes.ts
@@ -1,24 +1,24 @@
 import { Recipe } from '../../services/recipe/recipe';
 
 export const RECIPES: Recipe[] = [
-  // {
-  //   id: 1,
-  //   name: 'Toddy Cold Brew',
-  //   image: '../../../assets/img/toddy.jpg',
-  //   minutes: 0,
-  //   hours: 12,
-  //   days: 0,
-  //   description: 'Make delicious cold brew that although takes a day to make can make many servings.'
-  // },
-  // {
-  //   id: 2,
-  //   name: 'Quick Steel Cut Oatmeal',
-  //   image: '../../../assets/img/oatmeal.jpg',
-  //   minutes: 15,
-  //   hours: 0,
-  //   days: 0,
-  //   description: 'Make delicious cold brew that can serve many.'
-  // },
+  {
+    id: 1,
+    name: 'Toddy Cold Brew',
+    image: '../../../assets/img/toddy.jpg',
+    minutes: 0,
+    hours: 12,
+    days: 0,
+    description: 'Make delicious cold brew that although takes a day to make can make many servings.'
+  },
+  {
+    id: 2,
+    name: 'Quick Steel Cut Oatmeal',
+    image: '../../../assets/img/oatmeal.jpg',
+    minutes: 15,
+    hours: 0,
+    days: 0,
+    description: 'Make delicious cold brew that can serve many.'
+  },
   {
     id: 3,
     name: 'Baked Salmon',
@@ -28,22 +28,22 @@ export const RECIPES: Recipe[] = [
     days: 0,
     description: 'Make delicious cold brew that can serve many.'
   },
-  // {
-  //   id: 4,
-  //   name: 'Acai Bowl',
-  //   image: '../../../assets/img/acai.jpg',
-  //   minutes: 30,
-  //   hours: 0,
-  //   days: 0,
-  //   description: 'Make delicious cold brew that can serve many.'
-  // },
-  // {
-  //   id: 5,
-  //   name: 'V60 Pour Over Coffee',
-  //   image: '../../../assets/img/pour-over.jpg',
-  //   minutes: 15,
-  //   hours: 0,
-  //   days: 0,
-  //   description: 'Make delicious cold brew that can serve many.'
-  // }
+  {
+    id: 4,
+    name: 'Acai Bowl',
+    image: '../../../assets/img/acai.jpg',
+    minutes: 30,
+    hours: 0,
+    days: 0,
+    description: 'Make delicious cold brew that can serve many.'
+  },
+  {
+    id: 5,
+    name: 'V60 Pour Over Coffee',
+    image: '../../../assets/img/pour-over.jpg',
+    minutes: 15,
+    hours: 0,
+    days: 0,
+    description: 'Make delicious cold brew that can serve many.'
+  }
 ];
