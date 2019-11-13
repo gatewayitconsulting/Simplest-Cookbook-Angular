3a39e924df59ebc34e00df9ca8eb6757a3afb8fd
gatewayitconsultingDesktop <43389272+gatewayitconsulting@users.noreply.github.com>
2019-11-13 00:05:06 -0600

    Fixed search and search banner component responsiveness

--- simplest-cookbook/src/app/data/recipe/mock-recipes.ts
+++ simplest-cookbook/src/app/data/recipe/mock-recipes.ts
@@ -5,19 +5,19 @@ export const RECIPES: Recipe[] = [
     id: 1,
     name: 'Toddy Cold Brew',
     image: '../../../assets/img/toddy.jpg',
-    minutes: 0,
-    hours: 12,
-    days: 0,
-    description: 'Make delicious cold brew that although takes a day to make can make many servings.'
+    totalTime: 30,
+    description: 'Make quick and easy baked salmon that tastes fantastic.',
+    flavorProfile: 'Salt & Pepper',
+    category: 'Fish'
   },
   {
     id: 2,
     name: 'Quick Steel Cut Oatmeal',
     image: '../../../assets/img/oatmeal.jpg',
-    minutes: 15,
-    hours: 0,
-    days: 0,
-    description: 'Make delicious cold brew that can serve many.'
+    totalTime: 30,
+    description: 'Make quick and easy baked salmon that tastes fantastic.',
+    flavorProfile: 'Salt & Pepper',
+    category: 'Fish'
   },
   {
     id: 3,
@@ -32,18 +32,18 @@ export const RECIPES: Recipe[] = [
     id: 4,
     name: 'Acai Bowl',
     image: '../../../assets/img/acai.jpg',
-    minutes: 30,
-    hours: 0,
-    days: 0,
-    description: 'Make delicious cold brew that can serve many.'
+    totalTime: 30,
+    description: 'Make quick and easy baked salmon that tastes fantastic.',
+    flavorProfile: 'Salt & Pepper',
+    category: 'Fish'
   },
   {
     id: 5,
     name: 'V60 Pour Over Coffee',
     image: '../../../assets/img/pour-over.jpg',
-    minutes: 15,
-    hours: 0,
-    days: 0,
-    description: 'Make delicious cold brew that can serve many.'
+    totalTime: 30,
+    description: 'Make quick and easy baked salmon that tastes fantastic.',
+    flavorProfile: 'Salt & Pepper',
+    category: 'Fish'
   }
 ];
