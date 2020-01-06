dbc88043619e21ea402db76c7691f38050a15295
gatewayitconsultingDesktop <43389272+gatewayitconsulting@users.noreply.github.com>
2020-01-05 18:36:22 -0600

    Navigation Bug Smoothed Out
    Smoothed out navigation bug for all device sizes.
    Commented out maintanence and search component for production deployment.

--- simplest-cookbook/src/app/shared/header/header.component.ts
+++ simplest-cookbook/src/app/shared/header/header.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, HostListener, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { WINDOW } from '../../services/window/window';
-// import * as $ from 'jquery';
 
 declare var $: any;
 
@@ -22,9 +21,11 @@ export class HeaderComponent implements OnInit {
       const offset = this.window.pageYOffset || this.document.documentElement.scrollTop || this.document.body.scrollTop || 0;
       if (offset < 50) {
         $('nav').removeClass('scrolled');
+        $('button').removeClass('scrolled');
         $('span').removeClass('scrolled');
       } else {
         $('nav').addClass('scrolled');
+        $('button').addClass('scrolled');
         $('span').addClass('scrolled');
       }
   }
